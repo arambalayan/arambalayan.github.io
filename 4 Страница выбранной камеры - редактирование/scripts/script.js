746f401d3d4ad276916ea1bbb9ce09ab000209e4
arambalayan <aram199523@mail.ru>
2021-09-14 12:43:56 +0400

    14.09

--- 4 Страница выбранной камеры - редактирование/scripts/script.js
+++ 4 Страница выбранной камеры - редактирование/scripts/script.js
@@ -5,3 +5,43 @@ $(document).ready(function(){
 
     });
   });
+
+  
+
+$(document).ready(function(){
+    $(".lang-flag").click(function(){
+        $(".language-dropdown").toggleClass("open");
+        $(".language-dropdown").css({
+            "filter": "drop-shadow(0px 1px 4px rgba(0, 0, 0, 0.07))"
+    });
+    });
+    $("ul.lang-list li").click(function(){
+        $("ul.lang-list li").removeClass("selected");
+        $(this).addClass("selected");
+        if($(this).hasClass('lang-en')){
+            $(".language-dropdown").find(".lang-flag").addClass("lang-en").removeClass("lang-es").removeClass("lang-pt");
+            $("#rus").css("display", "flex")
+            $("#fran").css("display", "none")
+            $("#chin").css("display", "none")
+            $("#lang_selected").html("<p>EN</p>")
+        }else if($(this).hasClass('lang-pt')){
+            $(".language-dropdown").find(".lang-flag").addClass("lang-pt").removeClass("lang-es").removeClass("lang-en");
+            $("#rus").css("display", "none")
+            $("#fran").css("display", "flex")
+            $("#chin").css("display", "none")
+            $("#lang_selected").html("<p>PT</p>")
+        }else{
+            $(".language-dropdown").find(".lang-flag").addClass("lang-es").removeClass("lang-en").removeClass("lang-pt");
+            $("#rus").css("display", "none")
+            $("#fran").css("display", "none")
+            $("#chin").css("display", "flex")
+            $("#lang_selected").html("<p>ES</p>")
+        }
+        $(".language-dropdown").removeClass("open");
+        $(".language-dropdown").css({
+            "filter": "none"
+
+        });
+    });
+})
+
---
+++ header with modals/scripts/script.js
@@ -0,0 +1,67 @@
+$(function(){
+    $('.archive option:selected').hide(); //initialise
+
+    $('.archive').change(function(){
+
+        $('.archive option').show(200, function(){
+
+            $('.archive option:selected').hide();
+
+        });
+
+    });
+})
+$(document).ready(function(){
+    $(document).on('click','.signup-tab',function(e){
+        e.preventDefault();
+        $('#signup-taba').tab('show');
+        $('#loginModalLabel').hide();
+        $('#SignupModalLabel').show();
+    });
+
+    $(document).on('click','.signin-tab',function(e){
+        e.preventDefault();
+        $('#signin-taba').tab('show');
+        $('#loginModalLabel').show();
+        $('#SignupModalLabel').hide();
+    });
+
+    $(document).on('click','.forgetpass-tab',function(e){
+        e.preventDefault();
+        $('#forgetpass-taba').tab('show');
+        $('#SignupModalLabel').hide();
+        $('#loginModalLabel').hide();
+        $('#Forgetpassword').show();
+
+
+    });
+});
+
+$(document).ready(function(){
+    $(".lang-flag").click(function(){
+        $(".language-dropdown").toggleClass("open");
+        $(".language-dropdown").css({
+            "filter": "drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))"
+
+    });
+    });
+    $("ul.lang-list li").click(function(){
+        $("ul.lang-list li").removeClass("selected");
+        $(this).addClass("selected");
+        if($(this).hasClass('lang-en')){
+            $(".language-dropdown").find(".lang-flag").addClass("lang-en").removeClass("lang-es").removeClass("lang-pt");
+            $("#lang_selected").html("<p>EN</p>")
+        }else if($(this).hasClass('lang-pt')){
+            $(".language-dropdown").find(".lang-flag").addClass("lang-pt").removeClass("lang-es").removeClass("lang-en");
+            $("#lang_selected").html("<p>PT</p>")
+        }else{
+            $(".language-dropdown").find(".lang-flag").addClass("lang-es").removeClass("lang-en").removeClass("lang-pt");
+            $("#lang_selected").html("<p>ES</p>")
+        }
+        $(".language-dropdown").removeClass("open");
+        $(".language-dropdown").css({
+            "filter": "none"
+
+        });
+    });
+})
